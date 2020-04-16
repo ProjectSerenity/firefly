b027113d608ba9201da39c7abe2827d605cae85e
Jamie Hall <the.sly.marbo@googlemail.com>
2020-04-16 13:12:43 +0100

    Adjusted the base numerical types
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/std.h
+++ kernel/std.h
@@ -10,12 +10,16 @@ typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
 typedef unsigned long uint64;
-typedef unsigned int uint;
 
 typedef signed char int8;
 typedef signed short int16;
 typedef signed int int32;
 typedef signed long int64;
+
+typedef unsigned int uint;
+typedef unsigned long uintptr;
+typedef float float32;
+typedef double float64;
 
 const void* nil;
 
