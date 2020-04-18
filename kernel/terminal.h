0c5a67f50c8b6e00bee6978a39e1edfa6e6d4ec2
Jamie Hall <the.sly.marbo@googlemail.com>
2020-04-19 00:44:42 +0100

    Rewrite the terminal code to use VESA pixel drawing
    Previously we could make use of the BIOS's VGA text
    mode. Having jumped into long mode and with Pure64
    activating VESA graphics, we now have to draw text
    manually using a font.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/terminal.h
+++ kernel/terminal.h
@@ -2,31 +2,18 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
-// Hardware text mode color constants.
-enum vga_color {
-	VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-	VGA_COLOR_RED = 4,
-	VGA_COLOR_MAGENTA = 5,
-	VGA_COLOR_BROWN = 6,
-	VGA_COLOR_LIGHT_GREY = 7,
-	VGA_COLOR_DARK_GREY = 8,
-	VGA_COLOR_LIGHT_BLUE = 9,
-	VGA_COLOR_LIGHT_GREEN = 10,
-	VGA_COLOR_LIGHT_CYAN = 11,
-	VGA_COLOR_LIGHT_RED = 12,
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-	VGA_COLOR_LIGHT_BROWN = 14,
-	VGA_COLOR_WHITE = 15,
-};
+uint16 terminal_width;
+uint16 terminal_height;
+
+uint32 rgb(uint8 red, uint8 green, uint8 blue);
 
 void terminal_Init(void);
-void terminal_SetColor(uint8 color);
+void terminal_PixelAt(uint x, uint y, uint32 color);
+void terminal_CharAt(char c, uint32 color, uint x, uint y);
+void terminal_SetColor(uint32 color);
 uint terminal_Write(const char* data, uint size);
 uint terminal_WriteChar(char c);
-uint terminal_WriteCharAt(char c, uint8 color, uint x, uint y);
+uint terminal_WriteCharAt(char c, uint32 color, uint x, uint y);
 uint terminal_WriteString(string s);
 uint terminal_WriteError(string s);
 
