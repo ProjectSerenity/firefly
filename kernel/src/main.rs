b8761e8e82b33f9d27e77374f0270031b8487e73
Jamie Hall <the.sly.marbo@googlemail.com>
2020-06-30 21:02:45 +0100

    Got Hello, kernel! working in Rust

---
+++ kernel/src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    kmain();
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello, kernel!";
+
+fn kmain() {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+}
