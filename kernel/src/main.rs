598762829f7734241b297430cf562c101cf6b607
Jamie Hall <the.sly.marbo@googlemail.com>
2020-07-01 15:48:04 +0100

    Added function to halt the CPU, use in _start and panic.
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,21 +1,37 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
+#[cfg(target_arch = "x86_64")]
+fn halt() {
+    unsafe {
+        asm!("hlt");
+    }
+}
+
+// other platforms
+#[cfg(not(target_arch = "x86_64"))]
+fn halt() {}
+
 // This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    loop {
+        halt();
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     kmain();
-    loop {}
+    loop {
+        halt();
+    }
 }
 
 fn kmain() {
