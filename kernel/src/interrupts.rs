3f9fc6954de34359c371fa35bb601c4de0464d26
Jamie Hall <the.sly.marbo@googlemail.com>
2020-07-03 17:21:10 +0100

    Added code to set the global descriptor table
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -1,3 +1,4 @@
+use crate::gdt;
 use crate::println;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
@@ -10,6 +11,12 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
         idt
     };
 }
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -7,6 +7,7 @@
 
 use core::panic::PanicInfo;
 
+pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
@@ -14,6 +15,7 @@ pub mod vga_buffer;
 // init sets up critical core functions of the kernel.
 //
 pub fn init() {
+    gdt::init();
     interrupts::init();
 }
 
