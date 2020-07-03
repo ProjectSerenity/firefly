18bf81a384f62e3e55241b493ef31c0e779f4aa5
Jamie Hall <the.sly.marbo@googlemail.com>
2020-07-03 14:32:35 +0100

    Added unit tests for VGA buffer

--- kernel/src/vga_buffer.rs
+++ kernel/src/vga_buffer.rs
@@ -201,3 +201,27 @@ impl Writer {
         }
     }
 }
+
+// Tests
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
