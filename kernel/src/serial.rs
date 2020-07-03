4eedb2d57f4ec3129a9580cadfc96ed237dbda07
Jamie Hall <the.sly.marbo@googlemail.com>
2020-07-03 14:08:42 +0100

    Added a test framework

---
+++ kernel/src/serial.rs
@@ -0,0 +1,44 @@
+use lazy_static::lazy_static;
+use spin::Mutex;
+use uart_16550::SerialPort;
+
+lazy_static! {
+    pub static ref SERIAL1: Mutex<SerialPort> = {
+        let mut serial_port = unsafe { SerialPort::new(0x3F8) };
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+
+// _print writes text to the serial port by
+// acquiring SERIAL1 using a spin lock.
+//
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1
+        .lock()
+        .write_fmt(args)
+        .expect("Printing to serial failed");
+}
+
+// serial_print! prints to the host through
+// the serial interface.
+//
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+// serial_println! prints to the host through
+// the serial interface, appending a newline.
+//
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+        concat!($fmt, "\n"), $($arg)*));
+}
