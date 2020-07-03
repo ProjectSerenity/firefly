0a894d7bfa244bf742e964a54ac0888e3a03622a
Jamie Hall <the.sly.marbo@googlemail.com>
2020-07-03 14:08:42 +0100

    Added a test framework
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,16 +1,18 @@
 #![no_std]
 #![no_main]
-#![feature(asm)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+mod serial;
 mod vga_buffer;
 
 #[cfg(target_arch = "x86_64")]
 fn halt() {
-    unsafe {
-        asm!("hlt");
-    }
+    x86_64::instructions::interrupts::disable();
+    x86_64::instructions::hlt();
 }
 
 // other platforms
@@ -18,6 +20,7 @@ fn halt() {
 fn halt() {}
 
 // This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -28,12 +31,85 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
+
+    #[cfg(not(test))]
     kmain();
+
     loop {
         halt();
     }
 }
 
+#[cfg(not(test))]
 fn kmain() {
     println!("Hello, {}!", "kernel");
+}
+
+// Tests.
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
+// Testing framework.
+
+// This function is called on panic
+// when running tests.
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {
+        halt();
+    }
+}
+
+// Testable represents a test function.
+//
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+// Wrap tests with debug statements.
+//
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
 }
