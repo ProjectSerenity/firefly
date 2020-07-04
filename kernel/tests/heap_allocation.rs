fbcb6687c50611d999df50d00a43bf893836a4be
Jamie Hall <the.sly.marbo@googlemail.com>
2020-07-04 15:16:14 +0100

    Added integration tests for the heap allocator

---
+++ kernel/tests/heap_allocation.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(kernel::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+use alloc::{boxed::Box, vec::Vec};
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+use kernel::allocator::HEAP_SIZE;
+
+entry_point!(main);
+
+fn main(boot_info: &'static BootInfo) -> ! {
+    use kernel::allocator;
+    use kernel::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+
+    kernel::init();
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    test_main();
+    loop {}
+}
+
+#[test_case]
+fn simple_allocation() {
+    let heap_value_1 = Box::new(41);
+    let heap_value_2 = Box::new(13);
+    assert_eq!(*heap_value_1, 41);
+    assert_eq!(*heap_value_2, 13);
+}
+
+#[test_case]
+fn large_vec() {
+    let n = 1000;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+}
+
+#[test_case]
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kernel::test_panic_handler(info)
+}
