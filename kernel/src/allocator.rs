aacada2edc6bebf608aa828430232fd5ac243c04
Jamie Hall <the.sly.marbo@googlemail.com>
2020-07-04 16:05:57 +0100

    Switched to a fixed size block allocator

--- kernel/src/allocator.rs
+++ kernel/src/allocator.rs
@@ -1,10 +1,10 @@
-use linked_list_allocator::LockedHeap;
+use fixed_size_block::FixedSizeBlockAllocator;
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 use x86_64::VirtAddr;
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 mod bump;
 mod fixed_size_block;
