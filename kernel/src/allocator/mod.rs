ba76dc3a2a3cb18f9edd68e73f99b644b439e039
Jamie Hall <the.sly.marbo@googlemail.com>
2021-11-17 21:37:26 +0000

    Map the kernel's heap as non-executable
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/src/allocator/mod.rs
+++ kernel/src/allocator/mod.rs
@@ -42,7 +42,7 @@ pub fn init(
         let frame = frame_allocator
             .allocate_frame()
             .ok_or(MapToError::FrameAllocationFailed)?;
-        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::NO_EXECUTE;
         unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
     }
 
