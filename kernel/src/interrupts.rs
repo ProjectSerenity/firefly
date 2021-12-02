9a0252042212c7aae0ac62422a5ab145d52bcbec
Jamie Hall <the.sly.marbo@googlemail.com>
2021-12-02 19:00:45 +0000

    Added more interrupt handlers to aid debugging
    Several common CPU exceptions had no handler previously, leading
    to a double fault if they were triggered. This makes debugging
    difficult, as we don't know what exactly went wrong. This adds
    basic panicking handlers for them, so that at least we know the
    error more specifically.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -48,12 +48,17 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.page_fault.set_handler_fn(page_fault_handler);
+        idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
         unsafe {
             idt.double_fault
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt.segment_not_present
+            .set_handler_fn(segment_not_present_handler);
+        idt.general_protection_fault
+            .set_handler_fn(general_protection_fault_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
 
         idt[PIC_1_OFFSET + 0].set_handler_fn(irq_handler_0);
         idt[PIC_1_OFFSET + 1].set_handler_fn(irq_handler_1);
@@ -76,10 +81,45 @@ lazy_static! {
     };
 }
 
-// Interrupt handlers.
+// CPU exception handlers.
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: INVALID OPCODE\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn segment_not_present_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: SEGMENT NOT PRESENT: index {}\n{:#?}",
+        error_code, stack_frame
+    );
+}
+
+extern "x86-interrupt" fn general_protection_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    if error_code != 0 {
+        panic!(
+            "EXCEPTION: GENERAL PROTECTION FAULT: segment index {}\n{:#?}",
+            error_code, stack_frame
+        );
+    } else {
+        panic!("EXCEPTION: GENERAL PROTECTION FAULT:\n{:#?}", stack_frame);
+    }
 }
 
 extern "x86-interrupt" fn page_fault_handler(
@@ -93,12 +133,7 @@ extern "x86-interrupt" fn page_fault_handler(
     halt_loop();
 }
 
-extern "x86-interrupt" fn double_fault_handler(
-    stack_frame: InterruptStackFrame,
-    _error_code: u64,
-) -> ! {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
-}
+// IRQ handlers.
 
 fn timer_interrupt_handler(_stack_frame: InterruptStackFrame, _irq: u8) {
     time::tick();
