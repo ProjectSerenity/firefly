66e51f377b6d8e7c59b8e18982c7b81d117a0ad5
SlyMarbo <the.sly.marbo@googlemail.com>
2021-12-06 20:05:59 +0000

    Added a helper type to manage IRQ identifiers
    Previously, we would use simple u8s to refer to an IRQ. This worked
    fine, but we had to be careful to ensure we made the right choice
    between the logical IRQ number (in the range 0-15) or the interrupt
    identifier (which is offset by 32 to avoid overlapping with CPU
    exceptions.
    
    This commit introduces the Irq type, which wraps a u8 to enforce
    the constraints of the IRQ id, while including useful helper methods,
    such as to return the interrupt id, or to acknowledge the given IRQ.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -41,7 +41,7 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, Pag
 pub fn init() {
     IDT.load();
     unsafe { PICS.lock().initialize() };
-    register_irq(0, timer_interrupt_handler);
+    register_irq(Irq::new_unsafe(0), timer_interrupt_handler);
 }
 
 lazy_static! {
@@ -135,7 +135,7 @@ extern "x86-interrupt" fn page_fault_handler(
 
 // IRQ handlers.
 
-fn timer_interrupt_handler(_stack_frame: InterruptStackFrame, _irq: u8) {
+fn timer_interrupt_handler(_stack_frame: InterruptStackFrame, _irq: Irq) {
     time::tick();
 }
 
@@ -143,6 +143,66 @@ fn timer_interrupt_handler(_stack_frame: InterruptStackFrame, _irq: u8) {
 
 const PIC_1_OFFSET: usize = 32;
 const PIC_2_OFFSET: usize = PIC_1_OFFSET + 8;
+
+/// Irq represents an IRQ number.
+///
+/// It can be used to acknowledge the current IRQ from
+/// within the IRQ handler.
+///
+#[derive(Clone, Copy, Debug)]
+pub struct Irq(u8);
+
+impl Irq {
+    /// new returns an IRQ if within the range [0, 15], or None otherwise.
+    ///
+    pub fn new(irq: u8) -> Option<Irq> {
+        if irq <= 15 {
+            Some(Irq(irq))
+        } else {
+            None
+        }
+    }
+
+    /// new_unsafe returns an IRQ if within the range [0, 15], or panics
+    /// otherwise.
+    ///
+    pub fn new_unsafe(irq: u8) -> Irq {
+        if irq > 15 {
+            panic!("invalid IRQ larger than 15");
+        }
+
+        Irq(irq)
+    }
+
+    /// as_u8 returns the IRQ in the range [0, 15].
+    ///
+    pub fn as_u8(&self) -> u8 {
+        self.0
+    }
+
+    /// as_usize returns the IRQ in the range [0, 15].
+    ///
+    fn as_usize(&self) -> usize {
+        self.0 as usize
+    }
+
+    /// interrupt_id returns the IRQ as its offset in the interrupt table,
+    /// which will be offset from 0.
+    ///
+    pub fn interrupt_id(&self) -> u8 {
+        self.0 + PIC_1_OFFSET as u8
+    }
+
+    /// acknowledge informs the programmable interrupt controller that
+    /// this IRQ has been handled. Do not call acknowledge more than
+    /// once.
+    ///
+    pub fn acknowledge(&self) {
+        unsafe {
+            PICS.lock().notify_end_of_interrupt(self.interrupt_id());
+        }
+    }
+}
 
 /// PICS is the set of programmable interrupt controllers.
 ///
@@ -155,88 +215,85 @@ static PICS: spin::Mutex<ChainedPics> =
 ///
 /// The irq argument is an integer between 0 and 15.
 ///
-pub type IrqHandler = fn(frame: InterruptStackFrame, irq: u8);
+pub type IrqHandler = fn(frame: InterruptStackFrame, irq: Irq);
 
 /// irq_handler_none is a dummy IRQ handler, which
 /// does nothing.
 ///
-fn irq_handler_none(_frame: InterruptStackFrame, _irq: u8) {}
+fn irq_handler_none(_frame: InterruptStackFrame, _irq: Irq) {}
 
 // IRQ handlers.
 
 #[inline]
-fn irq_handler_generic(frame: InterruptStackFrame, irq: u8) {
-    let handler = IRQS.lock()[irq as usize];
+fn irq_handler_generic(frame: InterruptStackFrame, irq: Irq) {
+    let handler = IRQS.lock()[irq.as_usize()];
     handler(frame, irq);
 
-    unsafe {
-        PICS.lock()
-            .notify_end_of_interrupt(irq + PIC_1_OFFSET as u8);
-    }
+    irq.acknowledge();
 }
 
 extern "x86-interrupt" fn irq_handler_0(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 0u8);
+    irq_handler_generic(frame, Irq(0u8));
 }
 
 extern "x86-interrupt" fn irq_handler_1(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 1u8);
+    irq_handler_generic(frame, Irq(1u8));
 }
 
 extern "x86-interrupt" fn irq_handler_2(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 2u8);
+    irq_handler_generic(frame, Irq(2u8));
 }
 
 extern "x86-interrupt" fn irq_handler_3(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 3u8);
+    irq_handler_generic(frame, Irq(3u8));
 }
 
 extern "x86-interrupt" fn irq_handler_4(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 4u8);
+    irq_handler_generic(frame, Irq(4u8));
 }
 
 extern "x86-interrupt" fn irq_handler_5(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 5u8);
+    irq_handler_generic(frame, Irq(5u8));
 }
 
 extern "x86-interrupt" fn irq_handler_6(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 6u8);
+    irq_handler_generic(frame, Irq(6u8));
 }
 
 extern "x86-interrupt" fn irq_handler_7(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 7u8);
+    irq_handler_generic(frame, Irq(7u8));
 }
 
 extern "x86-interrupt" fn irq_handler_8(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 8u8);
+    irq_handler_generic(frame, Irq(8u8));
 }
 
 extern "x86-interrupt" fn irq_handler_9(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 9u8);
+    irq_handler_generic(frame, Irq(9u8));
 }
 
 extern "x86-interrupt" fn irq_handler_10(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 10u8);
+    irq_handler_generic(frame, Irq(10u8));
 }
 
 extern "x86-interrupt" fn irq_handler_11(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 11u8);
+    irq_handler_generic(frame, Irq(11u8));
 }
 
 extern "x86-interrupt" fn irq_handler_12(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 12u8);
+    irq_handler_generic(frame, Irq(12u8));
 }
 
 extern "x86-interrupt" fn irq_handler_13(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 13u8);
+    irq_handler_generic(frame, Irq(13u8));
 }
 
 extern "x86-interrupt" fn irq_handler_14(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 14u8);
+    irq_handler_generic(frame, Irq(14u8));
 }
 
 extern "x86-interrupt" fn irq_handler_15(frame: InterruptStackFrame) {
-    irq_handler_generic(frame, 15u8);
+    irq_handler_generic(frame, Irq(15u8));
 }
 
 /// IRQS helps us to track which IRQs have been allocated.
@@ -250,18 +307,14 @@ static IRQS: spin::Mutex<[IrqHandler; 16]> = spin::Mutex::new([irq_handler_none;
 /// If the given IRQ has already been assigned, register_irq
 /// panics.
 ///
-pub fn register_irq(irq: u8, handler: IrqHandler) {
-    if irq > 15 {
-        panic!("invalid IRQ {} passed to register_irq", irq);
-    }
-
+pub fn register_irq(irq: Irq, handler: IrqHandler) {
     interrupts::without_interrupts(|| {
         let mut irqs = IRQS.lock();
-        if irqs[irq as usize] != irq_handler_none {
-            panic!("IRQ {} has already been registered", irq);
+        if irqs[irq.as_usize()] != irq_handler_none {
+            panic!("IRQ {:?} has already been registered", irq);
         }
 
-        irqs[irq as usize] = handler;
+        irqs[irq.as_usize()] = handler;
     });
 }
 
