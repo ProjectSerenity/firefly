dcd021c38450d6048b74097cc24c7670d7aca33d
SlyMarbo <the.sly.marbo@googlemail.com>
2022-01-31 14:46:04 +0000

    storage: Added initial block storage device module
    This adds initial support for block storage devices. For now, this
    consists of a trait for devices, a private set of device drivers,
    and helper functions to invoke the first driver registered.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

---
+++ kernel/src/storage/block/mod.rs
@@ -0,0 +1,239 @@
+// Copyright 2022 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+//! Implements block storage devices for the kernel.
+
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+use bitflags::bitflags;
+use spin::Mutex;
+use x86_64::instructions::interrupts::without_interrupts;
+
+/// The list of block storage devices.
+///
+static DEVICES: Mutex<Vec<Box<dyn Device + Send>>> = Mutex::new(Vec::new());
+
+/// Registers a new block storage device.
+///
+pub fn add_device(device: Box<dyn Device + Send>) {
+    without_interrupts(|| {
+        let mut devices = DEVICES.lock();
+        devices.push(device);
+    });
+}
+
+// TODO: Select the device, rather than just using the first one.
+
+/// Returns the number of bytes in each segment.
+///
+pub fn segment_size() -> usize {
+    without_interrupts(|| {
+        let devices = DEVICES.lock();
+        match devices.get(0) {
+            None => 0,
+            Some(dev) => dev.segment_size(),
+        }
+    })
+}
+
+/// Returns the device capacity as a number of
+/// segments.
+//
+pub fn num_segments() -> usize {
+    without_interrupts(|| {
+        let devices = DEVICES.lock();
+        match devices.get(0) {
+            None => 0,
+            Some(dev) => dev.num_segments(),
+        }
+    })
+}
+
+/// Returns the device capacity in bytes.
+///
+pub fn capacity() -> usize {
+    without_interrupts(|| {
+        let devices = DEVICES.lock();
+        match devices.get(0) {
+            None => 0,
+            Some(dev) => dev.capacity(),
+        }
+    })
+}
+
+/// Returns the set of operations supported by the
+/// device.
+///
+/// If an unsupported operation is attempted, it
+/// will return [`Error::NotSupported`].
+///
+pub fn operations() -> Operations {
+    without_interrupts(|| {
+        let devices = DEVICES.lock();
+        match devices.get(0) {
+            None => Operations::empty(),
+            Some(dev) => dev.operations(),
+        }
+    })
+}
+
+/// Populates a byte slice with data from the device.
+///
+/// `segment` indicates from which segment the data
+/// should be read. The data read will start at the
+/// offset `segment` * [`segment_size`](segment_size).
+///
+/// Note that `buf` must have a length that is an exact
+/// multiple the [`segment_size`](segment_size).
+///
+/// `read` returns the number of bytes read.
+///
+pub fn read(segment: usize, buf: &mut [u8]) -> Result<usize, Error> {
+    without_interrupts(|| {
+        let mut devices = DEVICES.lock();
+        match devices.get_mut(0) {
+            None => Err(Error::NotSupported),
+            Some(dev) => dev.read(segment, buf),
+        }
+    })
+}
+
+/// Writes data from a byte slice to the device.
+///
+/// `segment` indicates from which segment the data
+/// should be read. The data written will start at the
+/// offset `segment` * [`segment_size`](segment_size).
+///
+/// Note that `buf` must have a length that is an exact
+/// multiple the [`segment_size`](segment_size).
+///
+/// `write` returns the number of bytes written.
+///
+/// If the device is read-only, calls to `write` will
+/// return [`Error::NotSupported`].
+///
+pub fn write(segment: usize, buf: &mut [u8]) -> Result<usize, Error> {
+    without_interrupts(|| {
+        let mut devices = DEVICES.lock();
+        match devices.get_mut(0) {
+            None => Err(Error::NotSupported),
+            Some(dev) => dev.write(segment, buf),
+        }
+    })
+}
+
+/// Flush the buffered data at the given `segment`.
+///
+/// `segment` indicates from which segment the data
+/// should be flushed. The data flushed will start at the
+/// offset `segment` * [`segment_size`](segment_size).
+///
+pub fn flush(segment: usize) -> Result<(), Error> {
+    without_interrupts(|| {
+        let mut devices = DEVICES.lock();
+        match devices.get_mut(0) {
+            None => Err(Error::NotSupported),
+            Some(dev) => dev.flush(segment),
+        }
+    })
+}
+
+bitflags! {
+    /// The list of operations that can be supported by
+    /// a device.
+    ///
+    pub struct Operations: usize {
+        /// Read data from a device.
+        const READ = 1 << 0;
+
+        /// Write data to a device.
+        const WRITE = 1 << 1;
+
+        /// Flush cached writes to a device.
+        const FLUSH = 1 << 2;
+    }
+}
+
+/// Describes an error encountered while operating
+/// on the device.
+///
+#[derive(Clone, Copy, Debug)]
+pub enum Error {
+    /// The buffer passed to the driver was not
+    /// appropriate for the operation.
+    InvalidBuffer,
+
+    /// The device encountered an error while performing
+    /// the requested operation.
+    DeviceError,
+
+    /// The requested operation is not supported.
+    NotSupported,
+
+    /// The device returned an invalid response.
+    BadResponse,
+}
+
+/// Represents a block storage device.
+///
+pub trait Device {
+    /// Returns the number of bytes in each segment.
+    ///
+    fn segment_size(&self) -> usize;
+
+    /// Returns the device capacity as a number of
+    /// segments.
+    //
+    fn num_segments(&self) -> usize;
+
+    /// Returns the device capacity in bytes.
+    ///
+    fn capacity(&self) -> usize;
+
+    /// Returns the set of operations supported by the
+    /// device.
+    ///
+    /// If an unsupported operation is attempted, it
+    /// will return [`Error::NotSupported`].
+    ///
+    fn operations(&self) -> Operations;
+
+    /// Populates a byte slice with data from the device.
+    ///
+    /// `segment` indicates from which segment the data
+    /// should be read. The data read will start at the
+    /// offset `segment` * [`segment_size`](Self::segment_size).
+    ///
+    /// Note that `buf` must have a length that is an exact
+    /// multiple the [`segment_size`](Self::segment_size).
+    ///
+    /// `read` returns the number of bytes read.
+    ///
+    fn read(&mut self, segment: usize, buf: &mut [u8]) -> Result<usize, Error>;
+
+    /// Writes data from a byte slice to the device.
+    ///
+    /// `segment` indicates from which segment the data
+    /// should be read. The data written will start at the
+    /// offset `segment` * [`segment_size`](Self::segment_size).
+    ///
+    /// Note that `buf` must have a length that is an exact
+    /// multiple the [`segment_size`](Self::segment_size).
+    ///
+    /// `write` returns the number of bytes written.
+    ///
+    /// If the device is read-only, calls to `write` will
+    /// return [`Error::NotSupported`].
+    ///
+    fn write(&mut self, segment: usize, buf: &mut [u8]) -> Result<usize, Error>;
+
+    /// Flush the buffered data at the given `segment`.
+    ///
+    /// `segment` indicates from which segment the data
+    /// should be flushed. The data flushed will start at the
+    /// offset `segment` * [`segment_size`](Self::segment_size).
+    ///
+    fn flush(&mut self, segment: usize) -> Result<(), Error>;
+}
