488990ff2ddef0d19f3ac9c5875a8476f7169b57
SlyMarbo <the.sly.marbo@googlemail.com>
2022-02-03 02:38:22 +0000

    task: Removed cooperative multitasking functionality
    It wasn't being used, so there's no reason to retain it.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/src/multitasking/mod.rs
+++ kernel/src/multitasking/mod.rs
@@ -5,13 +5,6 @@
 
 //! Implement cooperative and preemptive multitasking, plus CPU-local data.
 //!
-//! ## Cooperative multitasking
-//!
-//! Rust provides Futures and [`async`/`await`](https://rust-lang.github.io/async-book/01_getting_started/04_async_await_primer.html)
-//! as native cooperative multitasking features. The [`task`] module provides
-//! a simple Future wrapper and corresponding Executor to allow the safe use
-//! of `async` and `await` in the kernel.
-//!
 //! ## Preemptive multitasking
 //!
 //! The [`thread`] module implements Firefly threads, each of which has its
@@ -27,5 +20,4 @@
 //! CPU to have an independent copy of its own data.
 
 pub mod cpu_local;
-pub mod task;
 pub mod thread;
