026d7615feaeb2f0e941ca2265aa97769b795550
SlyMarbo <the.sly.marbo@googlemail.com>
2022-02-11 09:52:29 +0000

    cargo: Removed integration tests (should panic, stack overflow)
    There are a couple of reasons for removing these. Firstly, the
    should panic test doesn't achieve much. Secondly, I'm not sure
    the build system is the place to implement integration tests.
    I would rather do integration tests on the standard kernel, by
    varying the initial workload than building custom kernels.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -359,37 +359,6 @@ mod test {
     use alloc::collections::BTreeMap;
     use x86_64::structures::paging::PhysFrame;
 
-    #[test_case]
-    fn simple_allocation() {
-        use alloc::boxed::Box;
-        let heap_value_1 = Box::new(41);
-        let heap_value_2 = Box::new(13);
-        assert_eq!(*heap_value_1, 41);
-        assert_eq!(*heap_value_2, 13);
-    }
-
-    #[test_case]
-    fn large_vec() {
-        use alloc::vec::Vec;
-        let n = 1000;
-        let mut vec = Vec::new();
-        for i in 0..n {
-            vec.push(i);
-        }
-
-        assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
-    }
-
-    #[test_case]
-    fn many_boxes() {
-        use alloc::boxed::Box;
-        use memlayout::KERNEL_HEAP;
-        for i in 0..KERNEL_HEAP.size() {
-            let x = Box::new(i);
-            assert_eq!(*x, i);
-        }
-    }
-
     /// DebugPageTable is a helper type for testing code that
     /// uses page tables. It emulates the behaviour for a level
     /// 4 page table using heap memory, without modifying the
