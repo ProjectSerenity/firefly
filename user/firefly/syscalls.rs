f74b25c4c1797028cc5b1ba8d0ffbc7e2a46f5cc
SlyMarbo <the.sly.marbo@googlemail.com>
2022-04-17 19:50:32 +0100

    firefly: Added helper functions for calling raw syscalls
    The syscalls module will call lots of different syscalls, and we
    don't want to rewrite the assembly every time. Note that we make
    heavy use of named registers for both inputs and outputs, relying
    on the compiler to make this as efficient as possible. We also
    document the two registers that get clobbered.
    
    With the helpers defined, we can reimplement exit_thread by
    calling syscall0.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- user/firefly/syscalls.rs
+++ user/firefly/syscalls.rs
@@ -6,18 +6,205 @@
 //! Provides helper functions for calling Firefly syscalls.
 
 use core::arch::asm;
+use syscalls::Syscall;
+
+/// Call a raw syscall.
+///
+/// # Safety
+///
+/// This makes a raw syscall with unknown parameters,
+/// so it may have unsafe effects.
+///
+#[inline]
+pub unsafe fn syscall0(sys: usize) -> (usize, usize) {
+    let value: usize;
+    let error: usize;
+    asm! {
+        "syscall",
+        inlateout("rax") sys => value,
+        lateout("rdx") error,
+        out("rcx") _,
+        out("r11") _,
+    }
+
+    (value, error)
+}
+
+/// Call a raw syscall.
+///
+/// # Safety
+///
+/// This makes a raw syscall with unknown parameters,
+/// so it may have unsafe effects.
+///
+#[inline]
+pub unsafe fn syscall1(sys: usize, arg1: usize) -> (usize, usize) {
+    let value: usize;
+    let error: usize;
+    asm! {
+        "syscall",
+        inlateout("rax") sys => value,
+        in("rdi") arg1,
+        lateout("rdx") error,
+        out("rcx") _,
+        out("r11") _,
+    }
+
+    (value, error)
+}
+
+/// Call a raw syscall.
+///
+/// # Safety
+///
+/// This makes a raw syscall with unknown parameters,
+/// so it may have unsafe effects.
+///
+#[inline]
+pub unsafe fn syscall2(sys: usize, arg1: usize, arg2: usize) -> (usize, usize) {
+    let value: usize;
+    let error: usize;
+    asm! {
+        "syscall",
+        inlateout("rax") sys => value,
+        in("rdi") arg1,
+        in("rsi") arg2,
+        lateout("rdx") error,
+        out("rcx") _,
+        out("r11") _,
+    }
+
+    (value, error)
+}
+
+/// Call a raw syscall.
+///
+/// # Safety
+///
+/// This makes a raw syscall with unknown parameters,
+/// so it may have unsafe effects.
+///
+#[inline]
+pub unsafe fn syscall3(sys: usize, arg1: usize, arg2: usize, arg3: usize) -> (usize, usize) {
+    let value: usize;
+    let error: usize;
+    asm! {
+        "syscall",
+        inlateout("rax") sys => value,
+        in("rdi") arg1,
+        in("rsi") arg2,
+        inlateout("rdx") arg3 => error,
+        out("rcx") _,
+        out("r11") _,
+    }
+
+    (value, error)
+}
+
+/// Call a raw syscall.
+///
+/// # Safety
+///
+/// This makes a raw syscall with unknown parameters,
+/// so it may have unsafe effects.
+///
+#[inline]
+pub unsafe fn syscall4(
+    sys: usize,
+    arg1: usize,
+    arg2: usize,
+    arg3: usize,
+    arg4: usize,
+) -> (usize, usize) {
+    let value: usize;
+    let error: usize;
+    asm! {
+        "syscall",
+        inlateout("rax") sys => value,
+        in("rdi") arg1,
+        in("rsi") arg2,
+        inlateout("rdx") arg3 => error,
+        in("r10") arg4,
+        out("rcx") _,
+        out("r11") _,
+    }
+
+    (value, error)
+}
+
+/// Call a raw syscall.
+///
+/// # Safety
+///
+/// This makes a raw syscall with unknown parameters,
+/// so it may have unsafe effects.
+///
+#[inline]
+pub unsafe fn syscall5(
+    sys: usize,
+    arg1: usize,
+    arg2: usize,
+    arg3: usize,
+    arg4: usize,
+    arg5: usize,
+) -> (usize, usize) {
+    let value: usize;
+    let error: usize;
+    asm! {
+        "syscall",
+        inlateout("rax") sys => value,
+        in("rdi") arg1,
+        in("rsi") arg2,
+        inlateout("rdx") arg3 => error,
+        in("r10") arg4,
+        in("r8") arg5,
+        out("rcx") _,
+        out("r11") _,
+    }
+
+    (value, error)
+}
+
+/// Call a raw syscall.
+///
+/// # Safety
+///
+/// This makes a raw syscall with unknown parameters,
+/// so it may have unsafe effects.
+///
+#[inline]
+pub unsafe fn syscall6(
+    sys: usize,
+    arg1: usize,
+    arg2: usize,
+    arg3: usize,
+    arg4: usize,
+    arg5: usize,
+    arg6: usize,
+) -> (usize, usize) {
+    let value: usize;
+    let error: usize;
+    asm! {
+        "syscall",
+        inlateout("rax") sys => value,
+        in("rdi") arg1,
+        in("rsi") arg2,
+        inlateout("rdx") arg3 => error,
+        in("r10") arg4,
+        in("r8") arg5,
+        in("r9") arg6,
+        out("rcx") _,
+        out("r11") _,
+    }
+
+    (value, error)
+}
 
 /// Exit the current thread immediately.
 ///
 #[inline]
 pub fn exit_thread() -> ! {
-    unsafe {
-        // Call exit_thread (syscall 0).
-        asm! {
-            "xor rax, rax",
-            "syscall",
-        }
-    };
+    unsafe { syscall0(Syscall::ExitThread as usize) };
 
     unreachable!();
 }
