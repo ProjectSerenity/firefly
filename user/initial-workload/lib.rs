beb373f17d24ad572e87296cefc0b9a907f4d064
SlyMarbo <the.sly.marbo@googlemail.com>
2022-04-17 13:27:32 +0100

    initial-workload: Switch from assembly to Rust
    This has the same functionality, but implemented in Rust.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

---
+++ user/initial-workload/lib.rs
@@ -0,0 +1,21 @@
+// Copyright 2022 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+#![no_std]
+#![deny(clippy::inline_asm_x86_att_syntax)]
+#![deny(clippy::missing_panics_doc)]
+#![deny(clippy::panic)]
+#![deny(clippy::return_self_not_must_use)]
+#![deny(clippy::single_char_lifetime_names)]
+#![deny(clippy::wildcard_imports)]
+#![deny(unused_crate_dependencies)]
+#![deny(unsafe_code)]
+
+/// The application entry point.
+///
+#[inline]
+pub fn main() {
+    // TODO
+}
---
+++ user/initial-workload/main.rs
@@ -0,0 +1,33 @@
+// Copyright 2022 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+// This is a simple user application.
+
+#![no_std]
+#![no_main]
+#![deny(clippy::inline_asm_x86_att_syntax)]
+#![deny(clippy::missing_panics_doc)]
+#![deny(clippy::panic)]
+#![deny(clippy::return_self_not_must_use)]
+#![deny(clippy::single_char_lifetime_names)]
+#![deny(clippy::wildcard_imports)]
+#![deny(unused_crate_dependencies)]
+
+use core::panic::PanicInfo;
+use initial_workload::main;
+use syscalls::exit_thread;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// The application's entry point.
+#[no_mangle]
+pub extern "sysv64" fn _start() -> ! {
+    main();
+    exit_thread();
+}
