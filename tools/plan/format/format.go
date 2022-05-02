3387221564add79ae0739cf580a1a196ebbbc31c
SlyMarbo <the.sly.marbo@googlemail.com>
2022-05-02 21:53:13 +0100

    plan: Added an auto-formatter for the Plan interface language
    Plan is a minimal Lisp designed to be used to specify the ABI of
    an operating system. With the ABI defined in a Plan document,
    compatible code can be generated to either call or answer the
    corresponding system calls. This ensures that user and kernel
    code do not have different interpretations of the same data in
    memory.
    
    This includes an auto-formatter, which takes an abstract syntax
    tree and produces a Plan document containing the same information
    in the standard format. This also includes functionality to sort
    the fields defined in a Plan document into the standard order.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

---
+++ tools/plan/format/format.go
@@ -0,0 +1,303 @@
+// Copyright 2022 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+// Package format contains functionality to format a Plan document into the
+// canonical style.
+//
+package format
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"sort"
+
+	"github.com/ProjectSerenity/firefly/tools/plan/ast"
+	"github.com/ProjectSerenity/firefly/tools/plan/types"
+)
+
+// maxListWidth is the maximum width in bytes for a list.
+//
+// If a list is wider than this value, it will be split
+// into a vertical list with all entries after the first
+// indented by one. If the list is narrower than this,
+// it will be printed on one line, separated by a space.
+//
+const maxListWidth = 36
+
+// containsMultipleLists returns whether a set of expressions
+// contains more than one list.
+//
+// Alongside maxListWidth above, this is used to determine
+// whether to split a list onto multiple lines. We don't
+// want to have multiple lists on a single line, as it can
+// become quite hard to read.
+//
+func containsMultipleLists(elts []ast.Expr) bool {
+	lists := 0
+	for _, elt := range elts {
+		if _, ok := elt.(*ast.List); ok {
+			lists++
+			if lists > 1 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// SortFields ensures that the fields in each declaration
+// are sorted into the standard order.
+//
+// The list of fields, in order, includes:
+//
+// - name (all)
+// - docs (all)
+// - field (structure)
+// - type (enumeration, field, parameter)
+// - padding (field)
+// - value (enumeration)
+// - argN (syscall)
+// - resultN (syscall)
+//
+func SortFields(file *ast.File, arch types.Arch) error {
+	// Sort the fields before pretty-printing.
+	//
+	// This is easiest done by interpreting the
+	// file first, so that we can only sort the
+	// fields we care about and makes it easier
+	// to find those fields.
+	//
+	// It does mean we'll refuse to format files
+	// that are syntactically valid but have
+	// semantic errors, but this shouldn't matter
+	// much.
+	prog, err := types.Interpret("", file, arch)
+	if err != nil {
+		return err
+	}
+
+	// This is the order for fields.
+	order := map[string]int{
+		// These fields are always present and
+		// always comes first.
+		"name": 1,
+		"docs": 2,
+		// These fields are present in some
+		// types but not others.
+		"field":   3,
+		"type":    4,
+		"padding": 5,
+		"value":   6,
+		// Leave some space before these, which
+		// always come last.
+		"arg1":    20,
+		"arg2":    21,
+		"arg3":    22,
+		"arg4":    23,
+		"arg5":    24,
+		"arg6":    25,
+		"result1": 26,
+		"result2": 27,
+	}
+
+	sortList := func(typ string, list *ast.List) {
+		sort.Slice(list.Elements[1:], func(i, j int) bool {
+			namei := list.Elements[i+1].(*ast.List).Elements[0].(*ast.Identifier).Name
+			namej := list.Elements[j+1].(*ast.List).Elements[0].(*ast.Identifier).Name
+			priorityi := order[namei]
+			priorityj := order[namej]
+			if priorityi == 0 {
+				panic("unrecognised " + typ + " field: " + namei)
+			}
+			if priorityj == 0 {
+				panic("unrecognised " + typ + " field: " + namej)
+			}
+
+			return priorityi < priorityj
+		})
+	}
+
+	for _, enumeration := range prog.Enumerations {
+		sortList("enumeration", enumeration.Node)
+	}
+
+	for _, structure := range prog.Structures {
+		sortList("structure", structure.Node)
+		for _, field := range structure.Fields {
+			sortList("field", field.Node)
+		}
+	}
+
+	for _, syscall := range prog.Syscalls {
+		sortList("syscall", syscall.Node)
+		for _, arg := range syscall.Args {
+			sortList("arg", arg.Node)
+		}
+		for _, result := range syscall.Results {
+			sortList("result", result.Node)
+		}
+	}
+
+	return nil
+}
+
+// Fprint writes the file to w, according to the standard
+// style.
+//
+func Fprint(w io.Writer, file *ast.File) error {
+	allocated := false
+	var buf *bytes.Buffer
+	if b, ok := w.(*bytes.Buffer); ok {
+		buf = b
+	} else {
+		allocated = true
+		buf = new(bytes.Buffer)
+	}
+
+	// We don't know the order in which comments
+	// and expressions are interleaved, so we
+	// track the position of the next node and
+	// of each type and print the earlier of the
+	// two.
+	//
+	// We make a copy of the two slices so we
+	// can advance them to track our progress
+	// without modifying the file.
+	comments := make([]*ast.CommentGroup, len(file.Comments))
+	copy(comments, file.Comments)
+	lists := make([]*ast.List, len(file.Lists))
+	copy(lists, file.Lists)
+
+	nextNode := func() (comment *ast.CommentGroup, list *ast.List) {
+		if len(comments) == 0 {
+			// Must be a list.
+			list = lists[0]
+			lists = lists[1:]
+			return nil, list
+		} else if len(lists) == 0 {
+			// Must be a comment.
+			comment = comments[0]
+			comments = comments[1:]
+			return comment, nil
+		}
+
+		// Pick the one with the smaller
+		// offset.
+		nextComment := comments[0].Pos()
+		nextList := lists[0].Pos()
+		if nextComment.Offset() < nextList.Offset() {
+			comment = comments[0]
+			comments = comments[1:]
+			return comment, nil
+		}
+
+		list = lists[0]
+		lists = lists[1:]
+		return nil, list
+	}
+
+	first := true
+	for len(comments) != 0 || len(lists) != 0 {
+		comment, list := nextNode()
+		if first {
+			first = false
+		} else {
+			// Add a two-line gap between
+			// statements.
+			buf.WriteString("\n\n")
+		}
+
+		if comment != nil {
+			fprintCommentGroup(buf, 0, comment)
+		} else {
+			fprintExpr(buf, 0, list)
+		}
+
+		buf.WriteByte('\n')
+	}
+
+	if allocated {
+		_, err := w.Write(buf.Bytes())
+		return err
+	}
+
+	return nil
+}
+
+// 100 tabs.
+const tabs = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" +
+	"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" +
+	"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" +
+	"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"
+
+// fprintCommentGroup writes the comment group to buf, using
+// the simplify method to style the comments.
+//
+func fprintCommentGroup(buf *bytes.Buffer, indentation int, group *ast.CommentGroup) {
+	lines := group.Lines()
+	for i, line := range lines {
+		if i > 0 {
+			buf.WriteByte('\n')
+			buf.WriteString(tabs[:indentation])
+		}
+
+		if line == "" {
+			buf.WriteByte(';')
+		} else {
+			buf.WriteString("; ")
+			buf.WriteString(line)
+		}
+	}
+}
+
+// fprintExpr writes the node to buf, with the given indentation.
+//
+// fprintExpr does not write any spacing around the node.
+//
+func fprintExpr(buf *bytes.Buffer, indentation int, expr ast.Expr) {
+	switch x := expr.(type) {
+	case *ast.Identifier:
+		buf.WriteString(x.Name)
+	case *ast.String:
+		buf.WriteString(x.Text)
+	case *ast.Number:
+		buf.WriteString(x.Value)
+	case *ast.Pointer:
+		buf.WriteByte('*')
+		buf.WriteString(x.Note)
+	case *ast.List:
+		width := x.Width()
+		buf.WriteByte('(')
+		if width <= maxListWidth && !containsMultipleLists(x.Elements) {
+			// Nice and simple, all on one line.
+			for i, elt := range x.Elements {
+				if i > 0 {
+					buf.WriteByte(' ')
+				}
+
+				fprintExpr(buf, indentation+1, elt)
+			}
+		} else {
+			// The opening parenthesis and the first
+			// element go straight away, then we add
+			// a newline and indent for all subsequent
+			// elements.
+			for i, elt := range x.Elements {
+				if i > 0 {
+					buf.WriteByte('\n')
+					buf.WriteString(tabs[:indentation+1])
+				}
+
+				fprintExpr(buf, indentation+1, elt)
+			}
+		}
+
+		buf.WriteByte(')')
+	default:
+		panic(fmt.Sprintf("unexpected expression %#v", expr))
+	}
+}
---
+++ tools/plan/format/format_test.go
@@ -0,0 +1,198 @@
+// Copyright 2022 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+package format
+
+import (
+	"bytes"
+	"encoding/json"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/ProjectSerenity/firefly/tools/plan/parser"
+	"github.com/ProjectSerenity/firefly/tools/plan/types"
+)
+
+func TestFormatFile(t *testing.T) {
+	tests := []struct {
+		Name   string
+		Source string
+		Want   string
+	}{
+		{
+			Name:   "comment group",
+			Source: `; foo`,
+			Want:   `; foo`,
+		},
+		{
+			Name:   "compressed run of empty comments",
+			Source: ";foo\n;\n;\n; bar",
+			Want:   "; foo\n;\n; bar",
+		},
+		{
+			Name:   "preserved separate comment groups",
+			Source: "; foo\n\n; bar",
+			Want:   "; foo\n\n\n; bar",
+		},
+		{
+			Name: "complex example",
+			Source: `; Example
+; 123.
+
+(structure (field (docs "foo") (type byte)(name the first) ) (docs "quite a long string" "another string")(name asdf baz             example)  (field (docs "Extra padding.") (name spacer) (padding    3)))
+
+(enumeration (type uint8) (value (name bob) (docs "bob")) (name names) (docs "set of names") (value (name dave) (docs "dave")))
+
+; Another comment.
+
+(syscall (name read) (docs "read things")
+(arg2 (name size) (docs "length") (type uint64))
+(result2 (name error) (docs "what went wrong") (type uint64))
+(result1 (name name) (docs "who dun it") (type names))
+(arg1 (name ptr) (type *mutable byte) (docs "address")))`,
+			Want: `; Example
+; 123.
+
+
+(structure
+	(name asdf baz example)
+	(docs
+		"quite a long string"
+		"another string")
+	(field
+		(name the first)
+		(docs "foo")
+		(type byte))
+	(field
+		(name spacer)
+		(docs "Extra padding.")
+		(padding 3)))
+
+
+(enumeration
+	(name names)
+	(docs "set of names")
+	(type uint8)
+	(value
+		(name bob)
+		(docs "bob"))
+	(value
+		(name dave)
+		(docs "dave")))
+
+
+; Another comment.
+
+
+(syscall
+	(name read)
+	(docs "read things")
+	(arg1
+		(name ptr)
+		(docs "address")
+		(type *mutable byte))
+	(arg2
+		(name size)
+		(docs "length")
+		(type uint64))
+	(result1
+		(name name)
+		(docs "who dun it")
+		(type names))
+	(result2
+		(name error)
+		(docs "what went wrong")
+		(type uint64)))`,
+		},
+	}
+
+	var buf bytes.Buffer
+	var builder strings.Builder
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			buf.Reset()
+			file, err := parser.ParseFile("test.plan", test.Source)
+			if err != nil {
+				t.Fatalf("ParseFile(): %v", err)
+			}
+
+			err = SortFields(file, types.X86_64)
+			if err != nil {
+				t.Fatalf("SortFields(): %v", err)
+			}
+
+			err = Fprint(&buf, file)
+			if err != nil {
+				t.Fatalf("Fprint(): %v", err)
+			}
+
+			got := buf.String()
+			if got != test.Want+"\n" {
+				t.Fatalf("Fprint():\nGot:\n%q\nWant:\n%q", got, test.Want)
+			}
+
+			// Check that interpreting the original source and
+			// the formatted source gives the same result, as
+			// formatting should not result in semantic changes.
+
+			origParsed, err := parser.ParseFile("test.plan", test.Source)
+			if err != nil {
+				t.Fatalf("ParseFile(test.Source): %v", err)
+			}
+
+			origInterpreted, err := types.Interpret("test.plan", origParsed, types.X86_64)
+			if err != nil {
+				t.Fatalf("Interpret(test.Source): %v", err)
+			}
+
+			formattedParsed, err := parser.ParseFile("test.plan", got)
+			if err != nil {
+				t.Fatalf("ParseFile(formatted): %v", err)
+			}
+
+			formattedInterpreted, err := types.Interpret("test.plan", formattedParsed, types.X86_64)
+			if err != nil {
+				t.Fatalf("Interpret(formatted): %v", err)
+			}
+
+			// Ignore the AST nodes, as their positions will change.
+			origInterpreted.DropAST()
+			formattedInterpreted.DropAST()
+
+			if !reflect.DeepEqual(origInterpreted, formattedInterpreted) {
+				// Encoding the values in JSON makes the error
+				// message more useful and legible.
+				gotJSON, err := json.MarshalIndent(origInterpreted, "", "\t")
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				wantJSON, err := json.MarshalIndent(formattedInterpreted, "", "\t")
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				t.Fatalf("Interpret():\nGot:  %s\nWant: %s", gotJSON, wantJSON)
+			}
+
+			// Check that formatting the formatted code results
+			// in exactly the same sequence of bytes.
+
+			format1 := got
+
+			builder.Reset()
+			err = Fprint(&builder, formattedParsed)
+			if err != nil {
+				t.Fatalf("Fprint(formatted): %v", err)
+			}
+
+			format2 := builder.String()
+			if format2 != format1 {
+				t.Fatalf("Fprint(formatted):\nGot:  %q\nWant: %q", format2, format1)
+			}
+		})
+	}
+}
