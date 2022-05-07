ae3627625518b78679699782ecfd65e9c0a2136d
SlyMarbo <the.sly.marbo@googlemail.com>
2022-05-07 11:32:06 +0100

    plan: Added a full test case for RustKernelspace generation
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- tools/plan/rust_test.go
+++ tools/plan/rust_test.go
@@ -156,6 +156,86 @@ func TestRustUserspace(t *testing.T) {
 	}
 }
 
+func TestRustKernelspace(t *testing.T) {
+	tests := []struct {
+		Name string
+		Want string
+		Arch types.Arch
+		Text string
+	}{
+		{
+			Name: "Simple",
+			Want: "file_kernel_simple_rs.txt",
+			Arch: types.X86_64,
+			Text: `(structure
+			           (name file info)
+			           (docs "Information about a file in a filesystem.")
+			           (field
+			               (name name pointer)
+			               (docs "The pointer to the file's name contents.")
+			               (type *constant byte))
+			           (field
+			               (name name size)
+			               (docs "The number of bytes at 'name pointer'.")
+			               (type uint32)))
+
+			       (enumeration
+			           (name error)
+			           (docs "A general purpose error.")
+			           (type uint64)
+			           (value
+			               (name no error)
+			               (docs "No error occurred.")))
+
+			       (syscall
+			           (name deny syscalls)
+			           (docs "Denies the process access to the specified syscalls.")
+			           (arg1
+			               (name syscalls)
+			               (docs "The syscalls to deny.")
+			               (type syscalls))
+			           (result1
+			               (name error)
+			               (docs "Any error encountered.")
+			               (type error)))`,
+		},
+	}
+
+	rustfmt := getRustfmt(t)
+
+	var buf bytes.Buffer
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			buf.Reset()
+			parsed, err := parser.ParseFile("test.plan", test.Text)
+			if err != nil {
+				t.Fatalf("failed to parse: %v", err)
+			}
+
+			typed, err := types.Interpret("test.plan", parsed, test.Arch)
+			if err != nil {
+				t.Fatalf("failed to interpret: %v", err)
+			}
+
+			err = RustKernelspace(&buf, typed, rustfmt)
+			if err != nil {
+				t.Fatalf("failed to translate: %v", err)
+			}
+
+			wantName := filepath.Join("testdata", test.Want)
+			want, err := os.ReadFile(wantName)
+			if err != nil {
+				t.Fatalf("failed to read %q: %v", wantName, err)
+			}
+
+			got := buf.Bytes()
+			if !bytes.Equal(got, want) {
+				t.Fatalf("RustKernelspace(): output mismatch:\nGot:\n%s\nWant:\n%s", got, want)
+			}
+		})
+	}
+}
+
 func TestDefineRust(t *testing.T) {
 	tests := []struct {
 		Name string
