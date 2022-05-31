a7d048a1397c29d6bcdb5d2d7dc93b00f47cc06f
SlyMarbo <the.sly.marbo@googlemail.com>
2022-05-31 09:40:34 +0100

    bootloader: Added the usual lint configurations
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- bootloader/src/lib.rs
+++ bootloader/src/lib.rs
@@ -16,6 +16,15 @@
 //! blog for an explanation.
 
 #![no_std]
+#![deny(clippy::float_arithmetic)]
+#![deny(clippy::inline_asm_x86_att_syntax)]
+#![deny(clippy::missing_panics_doc)]
+#![deny(clippy::panic)]
+#![deny(clippy::return_self_not_must_use)]
+#![deny(clippy::single_char_lifetime_names)]
+#![deny(clippy::wildcard_imports)]
+#![deny(unused_crate_dependencies)]
+#![forbid(unsafe_code)]
 
 pub use crate::bootinfo::BootInfo;
 
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -10,6 +10,15 @@
 
 #![no_std]
 #![no_main]
+#![deny(clippy::float_arithmetic)]
+#![deny(clippy::inline_asm_x86_att_syntax)]
+#![deny(clippy::missing_panics_doc)]
+#![deny(clippy::panic)]
+#![deny(clippy::return_self_not_must_use)]
+#![deny(clippy::single_char_lifetime_names)]
+#![deny(clippy::wildcard_imports)]
+#![deny(unused_crate_dependencies)]
+#![allow(unsafe_code)]
 
 use bootloader::bootinfo::{BootInfo, FrameRange};
 use core::arch::asm;
