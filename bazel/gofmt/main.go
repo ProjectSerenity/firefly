743a0ad5ebd6087a43ba04169ad6cd6173f35a53
SlyMarbo <the.sly.marbo@googlemail.com>
2022-06-03 16:34:42 +0100

    gofmt: Added a packagedoc comment
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- bazel/gofmt/main.go
+++ bazel/gofmt/main.go
@@ -3,6 +3,8 @@
 // Use of this source code is governed by a BSD 3-clause
 // license that can be found in the LICENSE file.
 
+// Command gofmt checks whether Go code is formatted, exiting with an error if not.
+//
 package main
 
 import (
