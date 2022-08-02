0e2451e0ce6aac2097afe96adbcc407f0dd73a49
SlyMarbo <the.sly.marbo@googlemail.com>
2022-08-02 22:00:10 +0100

    vendeps: Added the ability to specify the test size
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- tools/vendeps/vendeps.go
+++ tools/vendeps/vendeps.go
@@ -113,6 +113,7 @@ type GoPackage struct {
 
 	// Test configuration.
 	NoTests       bool     `bzl:"no_tests"`
+	TestSize      string   `bzl:"test_size"`
 	TestData      []string `bzl:"test_data"`
 	TestDataGlobs []string `bzl:"test_data_globs"`
 	TestDeps      []string `bzl:"test_deps"`
