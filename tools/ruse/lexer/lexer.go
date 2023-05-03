0fff303b8f41a4dc49fe22bb8aa09291b58f7b5d
SlyMarbo <the.sly.marbo@googlemail.com>
2023-05-03 19:24:27 +0100

    ruse: Add the Ruse language lexer
    This adds a simple but effective lexer for the Ruse language,
    along with tests. As the syntax is simple, the lexer is also
    simple.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

---
+++ tools/ruse/lexer/lexer.go
@@ -0,0 +1,461 @@
+// Copyright 2023 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+// Package lexer implements a lexical scanner for Ruse source text.
+//
+// It takes a []byte source, which is then tokenised by repeated
+// calls to the Scan method.
+package lexer
+
+import (
+	"fmt"
+	"unicode"
+	"unicode/utf8"
+
+	"firefly-os.dev/tools/ruse/token"
+)
+
+// Lexeme describes a token, its position, and its textual
+// value.
+type Lexeme struct {
+	Token    token.Token
+	Position token.Pos
+	Value    string
+}
+
+func (l Lexeme) String(fset *token.FileSet) string {
+	return fmt.Sprintf("%s: %s (%s)", fset.Position(l.Position), l.Token, l.Value)
+}
+
+// lexer scans a sequence of bytes, producing a sequence of Ruse
+// tokens.
+type lexer struct {
+	// Immutable state.
+	src     []byte
+	file    *token.File
+	lexemes chan<- Lexeme
+
+	// Mutable state as we progress through the source.
+	offset     int // Offset into the file where the current token starts.
+	nextOffset int // Offset into the file of the current location.
+	width      int // Number of bytes in the last code point read.
+}
+
+// Scan scans the given Ruse source, producing a sequence of
+// lexical tokens.
+//
+// Once the end of the file is reached, or an error is encountered,
+// the channel will be closed, resulting in an endless sequence
+// of EndOfFile tokens.
+func Scan(file *token.File, source []byte) <-chan Lexeme {
+	c := make(chan Lexeme)
+	l := &lexer{
+		src:     source,
+		file:    file,
+		lexemes: c,
+
+		offset:     0,
+		nextOffset: 0,
+		width:      0,
+	}
+
+	go l.run()
+
+	return c
+}
+
+// run scans through the lexer's source, emitting tokens
+// until the end of the file is reached or an error is
+// encountered. In either case, the channel of lexemes
+// is then closed.
+func (l *lexer) run() {
+	// Close the channel, producing an endless sequence
+	// of EndOfFile tokens once we're done.
+	defer close(l.lexemes)
+
+	for {
+		// Skip over any whitespace.
+		for isWhitespace(l.next()) {
+		}
+
+		l.backup()
+		l.advance()
+
+		// Read the next rune.
+		r := l.next()
+		if r == eof {
+			return
+		}
+
+		switch {
+		case r == '(':
+			l.lexeme(token.ParenOpen)
+		case r == ')':
+			l.lexeme(token.ParenClose)
+		case r == '.':
+			// A period on its own is treated like an
+			// identifier. It must be on its own, or
+			// it would be ambiguous with a qualified
+			// identifier (which uses the Period token).
+			//
+			// Check the next token is appropriate; a
+			// closing parenthesis, comment, or space.
+			l.detatchedLexeme(token.Identifier, l.next())
+		case r == '-', r == '+':
+			// These are ambiguous so we need to read
+			// the next rune.
+			if isDigit(l.next()) {
+				l.backup()
+				l.scanNumber(r)
+				break
+			}
+
+			l.backup()
+
+			// Identifier handling.
+			fallthrough
+		case isIdentifierInitial(r):
+			// Keep going until we get a non-identifier
+			// token.
+			for r = l.next(); isIdentifierSubsequent(r); r = l.next() {
+			}
+
+			// Check for a qualified identifier.
+			if r == '.' {
+				l.backup()
+				l.lexeme(token.Identifier)
+				l.next()
+				l.lexeme(token.Period)
+				r = l.next()
+				if !isIdentifierInitial(r) {
+					l.errorf("invalid token %q after %s", r, token.Period)
+					continue
+				}
+
+				for r = l.next(); isIdentifierSubsequent(r); r = l.next() {
+				}
+			}
+
+			// Check the next token is appropriate; a
+			// closing parenthesis, comment, or space.
+			l.detatchedLexeme(token.Identifier, r)
+		case isDigit(r):
+			l.scanNumber(r)
+		case r == '"':
+			l.scanString()
+		case r == '\'':
+			// Check the next token is appropriate; an
+			// opening parenthesis, identifier, or integer.
+			l.atatchedLexeme(token.Quote, l.next())
+		case r == ';':
+			// Keep going until the end of the line or
+			// the end of the file, whichever comes
+			// first.
+			for r = l.next(); r != eof && r != '\n'; r = l.next() {
+			}
+
+			// Don't include the trailing newline.
+			if r == '\n' {
+				l.backup()
+			}
+
+			l.lexeme(token.Comment)
+		default:
+			l.errorf("invalid token %q", r)
+		}
+	}
+}
+
+// End of file pseudo-rune.
+const eof = -1
+
+// eof returns whether the lexer has reached the end
+// of the source.
+func (l *lexer) eof() bool {
+	return l.nextOffset >= len(l.src)
+}
+
+// errorf records the given error message.
+func (l *lexer) errorf(format string, v ...any) {
+	pos := l.file.Pos(l.offset)
+	l.lexemes <- Lexeme{Token: token.Error, Position: pos, Value: fmt.Sprintf(format, v...)}
+}
+
+// next consumes the next code point, returning it.
+func (l *lexer) next() (r rune) {
+	if l.eof() {
+		l.width = 0
+		return eof
+	}
+
+	// Try an ASCII character first.
+	r, l.width = rune(l.src[l.nextOffset]), 1
+	if r >= utf8.RuneSelf {
+		// Not ASCII.
+		r, l.width = utf8.DecodeRune(l.src[l.nextOffset:])
+		if r == utf8.RuneError && l.width == 1 {
+			l.errorf("source is not valid UTF-8")
+		}
+	}
+
+	l.nextOffset += l.width
+	if r == '\n' {
+		l.file.AddLine(l.nextOffset)
+	}
+
+	if r == 0 {
+		l.errorf("illegal character NUL")
+		return eof
+	}
+
+	return r
+}
+
+// backup steps back by one rune.
+//
+// If next was not called since the last call to
+// backup, peek, or Init, backup will panic.
+func (l *lexer) backup() {
+	if l.width == 0 && !l.eof() {
+		panic("internal error: lexer.backup() called without preceeding call to Lexer.next()")
+	}
+
+	if l.nextOffset == 0 && l.eof() || l.width == 0 {
+		return
+	}
+
+	l.nextOffset -= l.width
+	l.width = 0
+}
+
+// advance the source position.
+func (l *lexer) advance() {
+	l.offset = l.nextOffset
+}
+
+// lexeme emits a Lexeme at the current position,
+// with the given token type.
+func (l *lexer) lexeme(tok token.Token) {
+	pos := l.file.Pos(l.offset)
+	val := string(l.src[l.offset:l.nextOffset])
+	l.lexemes <- Lexeme{Token: tok, Position: pos, Value: val}
+	l.advance()
+}
+
+// detatchedLexeme emits a Lexeme at the current
+// position, with the given token type and an
+// error if the next rune is not a closing
+// parenthesis, comment, or space.
+func (l *lexer) detatchedLexeme(tok token.Token, next rune) {
+	ok := isClosing(next) || (l.eof() && l.width == 0)
+	l.backup()
+	l.lexeme(tok)
+	if !ok {
+		r := l.next()
+		l.errorf("invalid attached token: %q after %s", r, tok)
+	}
+}
+
+// atatchedLexeme emits a Lexeme at the current
+// position, with the given token type and an
+// error if the next rune a comment, end of file,
+// or space.
+func (l *lexer) atatchedLexeme(tok token.Token, next rune) {
+	ok := next == '(' || isDigit(next) || isIdentifierInitial(next)
+	l.backup()
+	l.lexeme(tok)
+	if !ok {
+		r := l.next()
+		l.errorf("invalid detached token: %q after %s", r, tok)
+	}
+}
+
+// scanNumber is called after the opening digit/sign
+// has been scanned.
+func (l *lexer) scanNumber(r rune) {
+	if r == '+' || r == '-' {
+		r = l.next()
+	}
+
+	base := 10
+	var prefix string
+	if r == '0' {
+		// Either decimal zero, the start of a radix
+		// prefix, or a short (illegal) octal literal.
+		r = l.next()
+		switch r {
+		case 'x':
+			// Hexadecimal literal.
+			base = 16
+			prefix = "0x"
+		case 'b':
+			// Binary literal.
+			base = 2
+			prefix = "0b"
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			// Octal short literal.
+			l.errorf("invalid number: short octal literals are not supported")
+			return
+		default:
+			// A decimal zero.
+			//
+			// Check the next token is appropriate; a
+			// closing parenthesis, comment, or space.
+			l.detatchedLexeme(token.Integer, r)
+			return
+		}
+	}
+
+	// If we've had a radix prefix, we need at
+	// least one subsequent digit.
+	if prefix != "" {
+		r = l.next()
+		if digitVal(r) >= base {
+			if digitVal(r) < 16 {
+				l.errorf("invalid number: %q is not a valid digit in base %d", r, base)
+			} else {
+				l.errorf("invalid number: radix prefix %q is not followed by a valid digit", prefix)
+			}
+
+			return
+		}
+	}
+
+	r = l.next()
+	for digitVal(r) < base {
+		r = l.next()
+	}
+
+	// Check the next token is appropriate; a
+	// closing parenthesis, comment, or space.
+	ok := isClosing(r) || (l.eof() && l.width == 0)
+	l.backup()
+	l.lexeme(token.Integer)
+	if !ok {
+		r = l.next()
+		if digitVal(r) < 16 {
+			l.errorf("invalid number: %q is not a valid digit in base %d", r, base)
+		} else {
+			l.errorf("invalid attached token: %q after %s", r, token.Integer)
+		}
+	}
+}
+
+// scanString is called after the opening quote
+// has been scanned.
+func (l *lexer) scanString() {
+	for {
+		r := l.next()
+		switch r {
+		case '"':
+			l.detatchedLexeme(token.String, l.next())
+			return
+		case '\n', eof:
+			l.errorf("string literal not terminated")
+			return
+		case '\\':
+			// Handle an escape sequence.
+			var n int
+			var base, max uint32
+			switch r := l.next(); r {
+			// Special characters.
+			case 'a', 'b', 'f', 'n', 'r', 't', 'v', '\\', '"':
+				continue
+			// Hexadecimal literal.
+			case 'x':
+				n, base, max = 2, 16, 255
+			// Small Unicode code point hex literal.
+			case 'u':
+				n, base, max = 4, 16, unicode.MaxRune
+			// Large Unicode code point hex literal.
+			case 'U':
+				n, base, max = 8, 16, unicode.MaxRune
+			case eof:
+				l.errorf("escape sequence not terminated")
+				return
+			default:
+				l.errorf("unrecognised escape sequence character %q", r)
+				return
+			}
+
+			// Handle the values of the escape sequence.
+			var x uint32
+			for n > 0 {
+				r := l.next()
+				if r == eof {
+					l.errorf("escape sequence not terminated")
+					return
+				}
+
+				d := uint32(digitVal(r))
+				if d >= base {
+					l.errorf("illegal character %q in escape sequence", r)
+					return
+				}
+
+				x = x*base + d
+				n--
+			}
+
+			if x > max || 0xD800 <= x && x < 0xE000 {
+				l.errorf("escape sequence is not a valid Unicode code point")
+				return
+			}
+		}
+	}
+}
+
+// Rune predicates.
+
+func isLetter(r rune) bool {
+	return 'a' <= r && r <= 'z' || 'A' <= r && r <= 'Z' || r == '_' || r >= utf8.RuneSelf && unicode.IsLetter(r)
+}
+
+func isIdentifierInitial(r rune) bool {
+	if isLetter(r) {
+		return true
+	}
+
+	switch r {
+	case '!', '$', '%', '&', '*', '/', ':', '<', '=', '>', '?', '~', '_', '^', '|':
+		return true
+	}
+
+	return false
+}
+
+func isIdentifierSubsequent(r rune) bool {
+	return isIdentifierInitial(r) || isDigit(r) || r == '+' || r == '-'
+}
+
+func isDigit(r rune) bool {
+	return '0' <= r && r <= '9'
+}
+
+func isWhitespace(r rune) bool {
+	switch r {
+	case ' ', '\t', '\r', '\n':
+		return true
+	default:
+		return false
+	}
+}
+
+func isClosing(r rune) bool {
+	return r == ')' || r == ';' || isWhitespace(r)
+}
+
+func digitVal(r rune) int {
+	switch {
+	case '0' <= r && r <= '9':
+		return int(r - '0')
+	case 'a' <= r && r <= 'f':
+		return int(r - 'a' + 10)
+	case 'A' <= r && r <= 'F':
+		return int(r - 'A' + 10)
+	}
+
+	return 16 // larger than any legal digit val
+}
---
+++ tools/ruse/lexer/lexer_test.go
@@ -0,0 +1,536 @@
+// Copyright 2023 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+package lexer
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"firefly-os.dev/tools/ruse/token"
+)
+
+func TestLexeme_String(t *testing.T) {
+	tests := []struct {
+		Src  string
+		Want []string
+	}{
+		{
+			Src: "foo-bar",
+			Want: []string{
+				"test.ruse:1:1: identifier (foo-bar)",
+			},
+		},
+		{
+			Src: "0",
+			Want: []string{
+				"test.ruse:1:1: integer (0)",
+			},
+		},
+		{
+			Src: "4",
+			Want: []string{
+				"test.ruse:1:1: integer (4)",
+			},
+		},
+		{
+			Src: "123456789",
+			Want: []string{
+				"test.ruse:1:1: integer (123456789)",
+			},
+		},
+		{
+			Src: " 123456789 .",
+			Want: []string{
+				"test.ruse:1:2: integer (123456789)",
+				"test.ruse:1:12: identifier (.)",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		fset := token.NewFileSet()
+		file := fset.AddFile("test.ruse", -1, len(test.Src))
+		lexemes := Scan(file, []byte(test.Src))
+		got := make([]Lexeme, 0, len(test.Want))
+		for lexeme := range lexemes {
+			got = append(got, lexeme)
+		}
+
+		gotStrings := make([]string, len(got))
+		for i, got := range got {
+			gotStrings[i] = got.String(fset)
+		}
+
+		if diff := cmp.Diff(test.Want, gotStrings); diff != "" {
+			t.Errorf("Lexing %q: (-want, +got)\n%s", test.Src, diff)
+		}
+	}
+}
+
+func TestLexer(t *testing.T) {
+	tests := []struct {
+		Name string
+		Src  string
+		Want []Lexeme
+	}{
+		{
+			Name: "identifier",
+			Src:  "foo",
+			Want: []Lexeme{
+				{token.Identifier, 1, "foo"},
+			},
+		},
+		{
+			Name: "complex identifier",
+			Src:  "foo-bar!",
+			Want: []Lexeme{
+				{token.Identifier, 1, "foo-bar!"},
+			},
+		},
+		{
+			Name: "decimal zero",
+			Src:  "0",
+			Want: []Lexeme{
+				{token.Integer, 1, "0"},
+			},
+		},
+		{
+			Name: "decimal integer",
+			Src:  "4",
+			Want: []Lexeme{
+				{token.Integer, 1, "4"},
+			},
+		},
+		{
+			Name: "large decimal integer",
+			Src:  "123456789",
+			Want: []Lexeme{
+				{token.Integer, 1, "123456789"},
+			},
+		},
+		{
+			Name: "large hexadecimal integer",
+			Src:  "0xdeadbeef123",
+			Want: []Lexeme{
+				{token.Integer, 1, "0xdeadbeef123"},
+			},
+		},
+		{
+			Name: "large binary integer",
+			Src:  "0b1011010111",
+			Want: []Lexeme{
+				{token.Integer, 1, "0b1011010111"},
+			},
+		},
+		{
+			Name: "large negative binary integer",
+			Src:  "-0xdeadbeef123",
+			Want: []Lexeme{
+				{token.Integer, 1, "-0xdeadbeef123"},
+			},
+		},
+		{
+			Name: "large positive binary integer",
+			Src:  "+0b1011010111",
+			Want: []Lexeme{
+				{token.Integer, 1, "+0b1011010111"},
+			},
+		},
+		{
+			Name: "plus identifier",
+			Src:  "+",
+			Want: []Lexeme{
+				{token.Identifier, 1, "+"},
+			},
+		},
+		{
+			Name: "minus identifier",
+			Src:  "-",
+			Want: []Lexeme{
+				{token.Identifier, 1, "-"},
+			},
+		},
+		{
+			Name: "underscore identifier",
+			Src:  "_",
+			Want: []Lexeme{
+				{token.Identifier, 1, "_"},
+			},
+		},
+		{
+			Name: "underscore prefixed identifier",
+			Src:  "_foo",
+			Want: []Lexeme{
+				{token.Identifier, 1, "_foo"},
+			},
+		},
+		{
+			Name: "qualified identifier",
+			Src:  "foo.bar",
+			Want: []Lexeme{
+				{token.Identifier, 1, "foo"},
+				{token.Period, 4, "."},
+				{token.Identifier, 5, "bar"},
+			},
+		},
+		{
+			Name: "empty string",
+			Src:  `""`,
+			Want: []Lexeme{
+				{token.String, 1, `""`},
+			},
+		},
+		{
+			Name: "non-empty string",
+			Src:  `"foo"`,
+			Want: []Lexeme{
+				{token.String, 1, `"foo"`},
+			},
+		},
+		{
+			Name: "string with escape code",
+			Src:  `"\t"`,
+			Want: []Lexeme{
+				{token.String, 1, `"\t"`},
+			},
+		},
+		{
+			Name: "string with many escape codes",
+			Src:  `"\xff \u00ff \U00008a9e"`,
+			Want: []Lexeme{
+				{token.String, 1, `"\xff \u00ff \U00008a9e"`},
+			},
+		},
+		{
+			Name: "quoted identifier",
+			Src:  "'foo",
+			Want: []Lexeme{
+				{token.Quote, 1, "'"},
+				{token.Identifier, 2, "foo"},
+			},
+		},
+		{
+			Name: "quoted integer",
+			Src:  "'123",
+			Want: []Lexeme{
+				{token.Quote, 1, "'"},
+				{token.Integer, 2, "123"},
+			},
+		},
+		{
+			Name: "quoted list",
+			Src:  "'(123)",
+			Want: []Lexeme{
+				{token.Quote, 1, "'"},
+				{token.ParenOpen, 2, "("},
+				{token.Integer, 3, "123"},
+				{token.ParenClose, 6, ")"},
+			},
+		},
+		{
+			Name: "nil list",
+			Src:  "()",
+			Want: []Lexeme{
+				{token.ParenOpen, 1, "("},
+				{token.ParenClose, 2, ")"},
+			},
+		},
+		{
+			Name: "pair",
+			Src:  "(foo . baz)",
+			Want: []Lexeme{
+				{token.ParenOpen, 1, "("},
+				{token.Identifier, 2, "foo"},
+				{token.Identifier, 6, "."},
+				{token.Identifier, 8, "baz"},
+				{token.ParenClose, 11, ")"},
+			},
+		},
+		{
+			Name: "list of one element",
+			Src:  "(foo)",
+			Want: []Lexeme{
+				{token.ParenOpen, 1, "("},
+				{token.Identifier, 2, "foo"},
+				{token.ParenClose, 5, ")"},
+			},
+		},
+		{
+			Name: "list of two elements",
+			Src:  "(foo \"bar\")",
+			Want: []Lexeme{
+				{token.ParenOpen, 1, "("},
+				{token.Identifier, 2, "foo"},
+				{token.String, 6, `"bar"`},
+				{token.ParenClose, 11, ")"},
+			},
+		},
+		{
+			Name: "List with intermittent spacing",
+			Src:  " (\tfoo\n\r)",
+			Want: []Lexeme{
+				{token.ParenOpen, 2, "("},
+				{token.Identifier, 4, "foo"},
+				{token.ParenClose, 9, ")"},
+			},
+		},
+		{
+			Name: "comment",
+			Src:  "; foobar",
+			Want: []Lexeme{
+				{token.Comment, 1, "; foobar"},
+			},
+		},
+		{
+			Name: "identifier with abutting comment",
+			Src:  "foo; foobar",
+			Want: []Lexeme{
+				{token.Identifier, 1, "foo"},
+				{token.Comment, 4, "; foobar"},
+			},
+		},
+		{
+			Name: "multi-line comment",
+			Src:  ";foo\n;bar",
+			Want: []Lexeme{
+				{token.Comment, 1, ";foo"},
+				{token.Comment, 6, ";bar"},
+			},
+		},
+		{
+			Name: "illegal tokens",
+			Src:  "£ foo ]",
+			Want: []Lexeme{
+				{token.Error, 1, "invalid token '£'"},
+				{token.Identifier, 4, "foo"},
+				{token.Error, 8, "invalid token ']'"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			fset := token.NewFileSet()
+			file := fset.AddFile("test.ruse", -1, len(test.Src))
+			lexemes := Scan(file, []byte(test.Src))
+			got := make([]Lexeme, 0, len(test.Want))
+			for lexeme := range lexemes {
+				got = append(got, lexeme)
+			}
+
+			if diff := cmp.Diff(test.Want, got); diff != "" {
+				t.Errorf("Lexing %q: (-want, +got)\n%s", test.Src, diff)
+			}
+		})
+	}
+}
+
+func TestError(t *testing.T) {
+	tests := []struct {
+		Name string
+		Src  string
+		Want Lexeme
+	}{
+		{
+			Name: "empty file",
+			Src:  "",
+			Want: Lexeme{token.EndOfFile, 0, ""},
+		},
+		{
+			Name: "short octal literal",
+			Src:  "01",
+			Want: Lexeme{token.Error, 1, "invalid number: short octal literals are not supported"},
+		},
+		{
+			Name: "binary raix prefix with no digits",
+			Src:  "0b",
+			Want: Lexeme{token.Error, 1, "invalid number: radix prefix \"0b\" is not followed by a valid digit"},
+		},
+		{
+			Name: "hexadecimal raix prefix with no digits",
+			Src:  "0x",
+			Want: Lexeme{token.Error, 1, "invalid number: radix prefix \"0x\" is not followed by a valid digit"},
+		},
+		{
+			Name: "binary literal with invalid digit",
+			Src:  "0b2",
+			Want: Lexeme{token.Error, 1, "invalid number: '2' is not a valid digit in base 2"},
+		},
+		{
+			Name: "binary literal with invalid subsequent digit",
+			Src:  "0b12",
+			Want: Lexeme{token.Error, 4, "invalid number: '2' is not a valid digit in base 2"},
+		},
+		{
+			Name: "integer followed by letter",
+			Src:  "1z",
+			Want: Lexeme{token.Error, 2, "invalid attached token: 'z' after integer"},
+		},
+		{
+			Name: "unterminated string literal",
+			Src:  `"1`,
+			Want: Lexeme{token.Error, 1, "string literal not terminated"},
+		},
+		{
+			Name: "multi-line string",
+			Src: `"1
+			          "`,
+			Want: Lexeme{token.Error, 1, "string literal not terminated"},
+		},
+		{
+			Name: "unterminated escape sequence in string",
+			Src:  `"\`,
+			Want: Lexeme{token.Error, 1, "escape sequence not terminated"},
+		},
+		{
+			Name: "invalid escape sequence character",
+			Src:  `"\p"`,
+			Want: Lexeme{token.Error, 1, "unrecognised escape sequence character 'p'"},
+		},
+		{
+			Name: "partial escape sequence",
+			Src:  `"\xf`,
+			Want: Lexeme{token.Error, 1, "escape sequence not terminated"},
+		},
+		{
+			Name: "truncated escape sequence",
+			Src:  `"\xF"`,
+			Want: Lexeme{token.Error, 1, "illegal character '\"' in escape sequence"},
+		},
+		{
+			Name: "invalid Unicode escape sequence",
+			Src:  `"\ud812"`,
+			Want: Lexeme{token.Error, 1, "escape sequence is not a valid Unicode code point"},
+		},
+		{
+			Name: "string literal with abutting identifier",
+			Src:  `"foo"a`,
+			Want: Lexeme{token.Error, 6, "invalid attached token: 'a' after string"},
+		},
+		{
+			Name: "period with abutting string literal",
+			Src:  ".\"foo\"",
+			Want: Lexeme{token.Error, 2, "invalid attached token: '\"' after identifier"},
+		},
+		{
+			Name: "unattached quote",
+			Src:  "' foo",
+			Want: Lexeme{token.Error, 2, "invalid detached token: ' ' after quote"},
+		},
+		{
+			Name: "identifier with abutting string literal",
+			Src:  "a\"foo\"",
+			Want: Lexeme{token.Error, 2, "invalid attached token: '\"' after identifier"},
+		},
+		{
+			Name: "qualified identifier with abutting string literal",
+			Src:  "a.\"foo\"",
+			Want: Lexeme{token.Error, 3, "invalid token '\"' after period"},
+		},
+		{
+			Name: "qualified identifier with abutting integer literal",
+			Src:  "a.4",
+			Want: Lexeme{token.Error, 3, "invalid token '4' after period"},
+		},
+		{
+			Name: "qualified identifier with detached identifier",
+			Src:  "a. b",
+			Want: Lexeme{token.Error, 3, "invalid token ' ' after period"},
+		},
+		{
+			Name: "unterminated string literal in list",
+			Src:  "(\"foo)",
+			Want: Lexeme{token.Error, 2, "string literal not terminated"},
+		},
+		{
+			Name: "NUL in string literal",
+			Src:  "(\"foo\x00\")",
+			Want: Lexeme{token.Error, 2, "illegal character NUL"},
+		},
+		{
+			Name: "invalid Unicode code point in string literal",
+			Src:  "(\"foo\x80\")",
+			Want: Lexeme{token.Error, 2, "source is not valid UTF-8"},
+		},
+		{
+			Name: "invalid token",
+			Src:  "£",
+			Want: Lexeme{token.Error, 1, "invalid token '£'"},
+		},
+		{
+			Name: "byte-order mark",
+			Src:  "foo \xfeff",
+			Want: Lexeme{token.Error, 4, "source is not valid UTF-8"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			fset := token.NewFileSet()
+			file := fset.AddFile("test.ruse", -1, len(test.Src))
+			lexemes := Scan(file, []byte(test.Src))
+			var got []Lexeme
+			for lexeme := range lexemes {
+				got = append(got, lexeme)
+			}
+
+			// Include an EndOfFile too.
+			got = append(got, <-lexemes)
+
+			// We may get other tokens after the first error,
+			// so we can't just check the last token.
+			ok := false
+			for _, lex := range got {
+				if lex == test.Want {
+					ok = true
+					break
+				}
+			}
+
+			if ok {
+				return
+			}
+
+			g := make([]string, len(got))
+			for i, got := range got {
+				g[i] = got.String(fset)
+			}
+
+			t.Errorf("Lexing %q:\nGot:\n  %s\nwant:\n  %s", test.Src, strings.Join(g, "\n  "), test.Want.String(fset))
+		})
+	}
+}
+
+func FuzzLexer(f *testing.F) {
+	tests := []string{
+		`x`,
+		`"foo"`,
+		`123`,
+		`*constant`,
+		`()`,
+		`; foo`,
+	}
+
+	for _, test := range tests {
+		f.Add([]byte(test))
+		f.Add([]byte(test + "\n"))
+	}
+
+	f.Fuzz(func(t *testing.T, input []byte) {
+		fset := token.NewFileSet()
+		file := fset.AddFile("test.ruse", -1, len(input))
+		lexemes := Scan(file, input)
+		for lexeme := range lexemes {
+			// Check that the token is one we recognise.
+			tok := lexeme.Token.String()
+			if strings.HasPrefix(tok, "Token(") {
+				t.Errorf("invalid token: %s", tok)
+			}
+		}
+	})
+}
