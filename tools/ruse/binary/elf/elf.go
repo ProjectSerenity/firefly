acbaede2c9cfdb162d448a83a930c49829866c17
SlyMarbo <the.sly.marbo@googlemail.com>
2023-05-03 19:45:54 +0100

    ruse: Added an ELF binary encoder
    This adds the functionality necessary to encode a
    Ruse binary in the ELF format. The implementation
    is fairly basic, but additional features should not
    be necessary. For example, Ruse is designed to work
    without an(other) operating system, so features
    like dynamic linking are not needed.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

---
+++ tools/ruse/binary/elf/elf.go
@@ -0,0 +1,140 @@
+// Copyright 2023 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+// Package elf encodes Ruse binaries according to the ELF
+// format.
+package elf
+
+import (
+	"bytes"
+	gobinary "encoding/binary"
+	"fmt"
+	"io"
+
+	"firefly-os.dev/tools/ruse/binary"
+	"firefly-os.dev/tools/ruse/sys"
+)
+
+// Encode writes the binary to w as an ELF binary.
+func Encode(w io.Writer, bin *binary.Binary) error {
+	write := false
+	b, ok := w.(*bytes.Buffer)
+	if !ok {
+		write = true
+		b = new(bytes.Buffer)
+	}
+
+	var err error
+	switch bin.Arch.PointerSize {
+	case 8:
+		err = encode64(b, bin)
+	default:
+		return fmt.Errorf("elf: %d-bit binaries are not supported", 8*bin.Arch.PointerSize)
+	}
+
+	if write && err == nil {
+		_, err = w.Write(b.Bytes())
+	}
+
+	return err
+}
+
+func endianness(bo gobinary.ByteOrder) uint8 {
+	switch bo.String() {
+	case "LittleEndian":
+		return 1
+	case "BigEndian":
+		return 2
+	}
+
+	return 0
+}
+
+func arch2machine(arch *sys.Arch) uint16 {
+	switch arch {
+	case sys.X86:
+		return 0x03
+	case sys.X86_64:
+		return 0x3e
+	}
+
+	return 0
+}
+
+func permissions(perm binary.Permissions) uint32 {
+	var out uint32
+	if perm.Read() {
+		out |= 0x04
+	}
+	if perm.Write() {
+		out |= 0x02
+	}
+	if perm.Execute() {
+		out |= 0x01
+	}
+
+	return out
+}
+
+func encode64(b *bytes.Buffer, bin *binary.Binary) error {
+	// See https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
+	bo := bin.Arch.ByteOrder
+	write := func(data any) {
+		gobinary.Write(b, bo, data)
+	}
+
+	progHeadOffs := 0x40                                  // Offset of the program headers.
+	progDataOffs := uint64(0x40 + 0x38*len(bin.Sections)) // Offset of the program data.
+	entry := uint64(bin.BaseAddr) + progDataOffs          // Entry point.
+
+	b.Write([]byte{0x7f, 'E', 'L', 'F'}) // Magic number.
+	b.WriteByte(2)                       // 64-bit format.
+	b.WriteByte(endianness(bo))          // Endianness.
+	b.WriteByte(1)                       // ELF version 1.
+	b.WriteByte(0x1e)                    // Firefly.
+	b.WriteByte(0)                       // ABI version.
+	b.Write(make([]byte, 7))             // Padding.
+	write(uint16(2))                     // Executable file.
+	write(arch2machine(bin.Arch))        // Architecture.
+	write(uint32(1))                     // ELF version 1.
+	write(entry)                         // Entry point address.
+	write(uint64(progHeadOffs))          // Program header table offset.
+	write(uint64(0))                     // Section header table offset (which we don't use).
+	write(uint32(0))                     // Flags (which we don't use).
+	write(uint16(0x40))                  // File header size.
+	write(uint16(0x38))                  // Program header size.
+	write(uint16(len(bin.Sections)))     // Number of program headers.
+	write(uint16(0x40))                  // Section header size.
+	write(uint16(0))                     // Number of section headers (which we don't use).
+	write(uint16(0))                     // Section header table index for section names (which we don't use).
+
+	// Add the program headers.
+	for _, section := range bin.Sections {
+		var fileSize uint64 // Size in the section.
+		if !section.IsZeroed {
+			fileSize = uint64(len(section.Data))
+		}
+
+		base := uint64(section.Address) + progDataOffs
+		write(uint32(1))                        // Loadable segment.
+		write(permissions(section.Permissions)) // Section flags.
+		write(progDataOffs)                     // File offset where segment begins.
+		write(base)                             // Section virtual address in memory.
+		write(base)                             // Section physical address in memory.
+		write(fileSize)                         // Size in the binary file.
+		write(uint64(len(section.Data)))        // Size in memory.
+		write(uint64(0x1000))                   // Alignment in memory.
+		progDataOffs += fileSize                // Skip over the section data in the file.
+	}
+
+	// Add the section data.
+	for _, section := range bin.Sections {
+		if !section.IsZeroed {
+			b.Write(section.Data)
+		}
+	}
+
+	return nil
+}
---
+++ tools/ruse/binary/elf/elf_test.go
@@ -0,0 +1,144 @@
+// Copyright 2023 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+package elf
+
+import (
+	"bytes"
+	"debug/elf"
+	gobinary "encoding/binary"
+	"io"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"firefly-os.dev/tools/ruse/binary"
+	"firefly-os.dev/tools/ruse/sys"
+)
+
+func TestEncode(t *testing.T) {
+	tests := []struct {
+		Name   string
+		Binary *binary.Binary
+		Want   *elf.File
+	}{
+		{
+			Name: "simple",
+			Binary: &binary.Binary{
+				Arch:     sys.X86_64,
+				BaseAddr: 0x0123456789abcdef,
+				Sections: []*binary.Section{
+					{
+						Name:        "code",
+						Address:     0x0123456789abcdef,
+						Permissions: binary.Read | binary.Execute,
+						Data:        []byte{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+					},
+					{
+						Name:        "data",
+						Address:     0xf1e2d3c4b5a60798,
+						IsZeroed:    true,
+						Permissions: binary.Read | binary.Write,
+						Data:        []byte{0xff},
+					},
+					{
+						Name:        "strings",
+						Address:     0xfedcba9876543210,
+						Permissions: binary.Read,
+						Data:        []byte("Hello, world!"),
+					},
+				},
+			},
+			Want: &elf.File{
+				FileHeader: elf.FileHeader{
+					Class:      elf.ELFCLASS64,
+					Data:       elf.ELFDATA2LSB,
+					Version:    elf.EV_CURRENT,
+					OSABI:      0x1e,
+					ABIVersion: 0,
+					ByteOrder:  gobinary.LittleEndian,
+					Type:       elf.ET_EXEC,
+					Machine:    elf.EM_X86_64,
+					Entry:      0x0123456789abcdef + 0x40 + 3*0x38,
+				},
+				Sections: []*elf.Section{},
+				Progs: []*elf.Prog{
+					{
+						ProgHeader: elf.ProgHeader{
+							Type:   elf.PT_LOAD,
+							Flags:  elf.PF_R | elf.PF_X,
+							Off:    0x40 + 3*0x38,
+							Vaddr:  0x0123456789abcdef + 0x40 + 3*0x38,
+							Paddr:  0x0123456789abcdef + 0x40 + 3*0x38,
+							Filesz: 10,
+							Memsz:  10,
+							Align:  0x1000,
+						},
+					},
+					{
+						ProgHeader: elf.ProgHeader{
+							Type:   elf.PT_LOAD,
+							Flags:  elf.PF_R | elf.PF_W,
+							Off:    0x40 + 3*0x38 + 10,
+							Vaddr:  0xf1e2d3c4b5a60798 + 0x40 + 3*0x38 + 10,
+							Paddr:  0xf1e2d3c4b5a60798 + 0x40 + 3*0x38 + 10,
+							Filesz: 0,
+							Memsz:  1,
+							Align:  0x1000,
+						},
+					},
+					{
+						ProgHeader: elf.ProgHeader{
+							Type:   elf.PT_LOAD,
+							Flags:  elf.PF_R,
+							Off:    0x40 + 3*0x38 + 10,
+							Vaddr:  0xfedcba9876543210 + 0x40 + 3*0x38 + 10,
+							Paddr:  0xfedcba9876543210 + 0x40 + 3*0x38 + 10,
+							Filesz: 13,
+							Memsz:  13,
+							Align:  0x1000,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	opts := []cmp.Option{
+		cmp.AllowUnexported(io.SectionReader{}, bytes.Reader{}),
+		cmpopts.IgnoreUnexported(elf.File{}, elf.Section{}, elf.Prog{}),
+	}
+
+	var b bytes.Buffer
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			b.Reset()
+			err := Encode(&b, test.Binary)
+			if err != nil {
+				t.Fatalf("failed to encode: %v", err)
+			}
+
+			raw := b.Bytes()
+			br := bytes.NewReader(raw)
+			// Sort out the expected ReadersAt.
+			for _, sect := range test.Want.Sections {
+				sect.ReaderAt = io.NewSectionReader(br, int64(sect.Offset), int64(sect.FileSize))
+			}
+			for _, prog := range test.Want.Progs {
+				prog.ReaderAt = io.NewSectionReader(br, int64(prog.Off), int64(prog.Filesz))
+			}
+
+			got, err := elf.NewFile(br)
+			if err != nil {
+				t.Fatalf("failed to parse: %v", err)
+			}
+
+			if diff := cmp.Diff(test.Want, got, opts...); diff != "" {
+				t.Fatalf("Encode(): (-want, +got)\n%s", diff)
+			}
+		})
+	}
+}
