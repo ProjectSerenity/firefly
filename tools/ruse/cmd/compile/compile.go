3e5ce7a73d152fc680732a004a0e07d967281b0b
SlyMarbo <the.sly.marbo@googlemail.com>
2023-07-07 23:47:57 +0100

    ruse: Change the compiler to produce rpkg files, not binaries
    Now that we have the rpkg file format, we can compile a package
    (and not only package main) to that, rather than needing to
    produce an executable binary. This means that many packages can
    be compiled individually and linked together later.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- tools/ruse/cmd/compile/compile.go
+++ tools/ruse/cmd/compile/compile.go
@@ -7,11 +7,9 @@
 package compile
 
 import (
-	"bytes"
 	"context"
 	"flag"
 	"fmt"
-	"go/constant"
 	"io"
 	"log"
 	"os"
@@ -19,18 +17,15 @@ import (
 	"sort"
 
 	"firefly-os.dev/tools/ruse/ast"
-	"firefly-os.dev/tools/ruse/binary"
-	"firefly-os.dev/tools/ruse/binary/elf"
 	"firefly-os.dev/tools/ruse/compiler"
 	"firefly-os.dev/tools/ruse/parser"
+	"firefly-os.dev/tools/ruse/rpkg"
 	"firefly-os.dev/tools/ruse/sys"
 	"firefly-os.dev/tools/ruse/token"
 	"firefly-os.dev/tools/ruse/types"
 )
 
 var program = filepath.Base(os.Args[0])
-
-type binaryEncoder func(w io.Writer, bin *binary.Binary) error
 
 // Main compiles a set of Ruse files into an executable
 // binary.
@@ -40,7 +35,6 @@ func Main(ctx context.Context, w io.Writer, args []string) error {
 	var help bool
 	var out string
 	var arch *sys.Arch
-	var encode binaryEncoder
 	flags.BoolVar(&help, "h", false, "Show this message and exit.")
 	flags.Func("arch", "The target architecture (x86-64).", func(s string) error {
 		if arch != nil {
@@ -56,21 +50,7 @@ func Main(ctx context.Context, w io.Writer, args []string) error {
 
 		return nil
 	})
-	flags.Func("binary", "The binary encoding (elf).", func(s string) error {
-		if encode != nil {
-			return fmt.Errorf("-binary can only be specified once")
-		}
-
-		switch s {
-		case "elf":
-			encode = elf.Encode
-		default:
-			return fmt.Errorf("unrecognised -binary format: %q", s)
-		}
-
-		return nil
-	})
-	flags.StringVar(&out, "o", "", "The name of the compiled binary.")
+	flags.StringVar(&out, "o", "", "The name of the compiled rpkg.")
 
 	flags.Usage = func() {
 		log.Printf("Usage:\n  %s %s OPTIONS FILE...\n\n", program, flags.Name())
@@ -83,7 +63,7 @@ func Main(ctx context.Context, w io.Writer, args []string) error {
 		flags.Usage()
 	}
 
-	if arch == nil || encode == nil || out == "" {
+	if arch == nil || out == "" {
 		flags.Usage()
 		os.Exit(2)
 	}
@@ -100,10 +80,6 @@ func Main(ctx context.Context, w io.Writer, args []string) error {
 		files[i], err = parser.ParseFile(fset, filename, nil, 0)
 		if err != nil {
 			return err
-		}
-
-		if files[i].Name.Name != "main" {
-			return fmt.Errorf("can only compile package main yet, found %s", files[i].Name.Name)
 		}
 	}
 
@@ -137,108 +113,14 @@ func Main(ctx context.Context, w io.Writer, args []string) error {
 		}
 	})
 
-	symbols := make(map[string]*binary.Symbol)
-	var table []*binary.Symbol
-	var code, stringsData bytes.Buffer
-	for _, fun := range p.Functions {
-		prev := code.Len()
-		sym := &binary.Symbol{
-			Name:    p.Path + "." + fun.Name,
-			Kind:    binary.SymbolFunction,
-			Section: 0,
-			Offset:  uintptr(prev), // Just the offset within the section for now.
-		}
-
-		err = compiler.EncodeTo(&code, fset, arch, fun)
-		if err != nil {
-			return err
-		}
-
-		sym.Length = code.Len() - prev
-		table = append(table, sym)
-		symbols[sym.Name] = sym
-	}
-
-	for _, con := range p.Constants {
-		val := con.Value()
-		if val.Kind() != constant.String {
-			// Non-string constants are inlined.
-			continue
-		}
-
-		s := constant.StringVal(val)
-		sym := &binary.Symbol{
-			Name:    p.Path + "." + con.Name(),
-			Kind:    binary.SymbolString,
-			Section: 1,
-			Offset:  uintptr(stringsData.Len()), // Just the offset within the section for now.
-			Length:  len(s),
-		}
-
-		stringsData.WriteString(s)
-		table = append(table, sym)
-		symbols[sym.Name] = sym
-	}
-
-	const page4k = 0x1000 // One 4 KiB page.
-	bin := &binary.Binary{
-		Arch:     arch,
-		BaseAddr: 0x20_0000, // 2 MiB in.
-		Sections: []*binary.Section{
-			{
-				Name:        "code",
-				Address:     0x20_0000,
-				Permissions: binary.Read | binary.Execute,
-				Data:        code.Bytes(),
-			},
-			{
-				Name:        "strings",
-				Address:     0x20_0000 + uintptr(code.Len()) + uintptr(page4k-(code.Len()%page4k)), // The start of the next page after code.
-				Permissions: binary.Read,
-				Data:        stringsData.Bytes(),
-			},
-		},
-	}
-
-	var b bytes.Buffer
-	err = encode(&b, bin)
+	f, err := os.Create(out)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create %s: %v", out, err)
 	}
 
-	object := b.Bytes()
-
-	// Finish the symbol table.
-	for _, sym := range table {
-		sym.Offset += bin.Sections[sym.Section].Offset
-		sym.Address = sym.Offset + bin.Sections[sym.Section].Address
-	}
-
-	// Perform any linkages.
-	for _, fun := range p.Functions {
-		// Get the function base.
-		absFunName := p.Path + "." + fun.Name
-		funSym := symbols[absFunName]
-		if funSym == nil {
-			return fmt.Errorf("internal error: failed to find symbol for %s", absFunName)
-		}
-
-		for _, link := range fun.Links {
-			sym := symbols[link.Name]
-			if sym == nil {
-				return fmt.Errorf("internal error: failed to find symbol for %s", link.Name)
-			}
-
-			err := link.Perform(arch, object, funSym, sym.Address)
-			if err != nil {
-				return fmt.Errorf("%s: %v", fset.Position(link.Pos), err)
-			}
-		}
-	}
-
-	err = os.WriteFile(out, object, 0755)
+	err = rpkg.Encode(f, fset, arch, p, info)
 	if err != nil {
-		return fmt.Errorf("failed to write %s: %v", out, err)
+		return fmt.Errorf("failed to compile %s: %v", out, err)
 	}
 
 	return nil
